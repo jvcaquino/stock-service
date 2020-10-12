f08884d5ece835520c70feaf813fb72db9bd50bc
jvcaquino <jvcaquino95@gmail.com>
2020-10-12 18:27:06 -0300

    Adding flyway/jdbi support and creating product base crud

---
+++ src/main/java/com/market/stock/dao/ProductRowMapper.java
@@ -0,0 +1,21 @@
+package com.market.stock.dao;
+
+import com.market.stock.domain.Product;
+import org.jdbi.v3.core.mapper.RowMapper;
+import org.jdbi.v3.core.statement.StatementContext;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class ProductRowMapper implements RowMapper<Product> {
+
+    public static final String PRODUCT_ID = "id";
+    public static final String PRODUCT_NAME = "name";
+
+    @Override
+    public Product map(ResultSet resultSet, StatementContext statementContext) throws SQLException {
+        return new Product(resultSet.getInt(PRODUCT_ID),
+                resultSet.getString(PRODUCT_NAME)
+        );
+    }
+}
---
+++ src/main/java/com/market/stock/domain/Product.java
@@ -0,0 +1,52 @@
+package com.market.stock.domain;
+
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.Objects;
+import java.util.StringJoiner;
+
+public class Product {
+
+    private int id;
+    private final String name;
+
+    @JsonCreator
+    public Product(@JsonProperty("name") String name) {
+        this.name = name;
+    }
+
+    public Product(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public String toString() {
+        return new StringJoiner(", ", Product.class.getSimpleName() + "[", "]")
+                .add("name='" + name + "'")
+                .toString();
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return id == product.id &&
+                Objects.equals(name, product.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+}
